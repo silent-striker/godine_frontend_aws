bfef94df23e71727759e817256650e65b6aefc5a
Priyatam Reddy Somagattu <pr889078@dal.ca>
2024-08-07 13:03:15 -0300

    updated packages and removed unnecessary

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { AuthProvider } from './context/AuthContext';
 import { SearchProvider } from './context/SearchContext'
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -16,8 +15,3 @@ root.render(
     </SearchProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
